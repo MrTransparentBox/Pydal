4808e784cf6f0089f7d4e6af555c7900bc9b99bb
Alex Johnson <alexjohnson2004@gmail.com>
2024-02-09 01:38:45 +0000

    Added poetry.lock, removed static path in tests

--- tests/main.py
+++ tests/main.py
@@ -5,13 +5,12 @@ import os
 import sys
 
 if __name__ == "__main__":
-
-    sys.path.append("C:\\Users\\alexj\\VSCode\\Python\\Pydal")
+    sys.path.append(os.getcwd())
     from tidal import TidalClient
 
     async def main():
         tidal = TidalClient(os.getenv("tidal_id"), os.getenv("tidal_secret"))
         track = await tidal.track("3146735", "GB")
-        print(track["title"], " url ", track["tidalUrl"])
+        print(f"Name: {track["title"]}\nURL: {track["tidalUrl"]}")
 
     asyncio.run(main())
